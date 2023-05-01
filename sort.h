38adab705968a93a586d23f81426107a8510219a
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-05-01 01:08:58 +0100

    Update

--- sort.h
+++ sort.h
@@ -1,10 +1,7 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
-#include <stdio.h>
+#ifndef SORT_H
+#define SORT_H
 
-
-/** Struct Double Linked List */
-
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,17 +16,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
-
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/** TASK Prototype */
-void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
